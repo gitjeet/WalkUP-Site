3a741d7dcec66e0b6c36564d2f4f3d30706c5ea5
abhijeet <abhijeet@5ire.org>
2024-01-28 12:33:41 +0530

    Make the code working

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import { light } from "./styles/Themes";
+import Navigation from "./components/Navigation";
+import Home from './components/sections/Home'
+import About from './components/sections/About'
+import Roadmap from './components/sections/Roadmap'
+import Showcase from './components/sections/Showcase'
+import Team from './components/sections/Team'
+import Faq from './components/sections/Faq'
+import Footer from './components/Footer'
+import ScrollToTop from "./components/ScrollToTop";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={light}>
+
+        <Navigation />
+        <Home/>
+        <About/>        
+        <Roadmap/>        
+        <Showcase/>        
+        <Team/>        
+        <Faq/>        
+        <Footer/>        
+        <ScrollToTop/>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
--- src/Icons/Arrow.js
+++ src/Icons/Arrow.js
@@ -1,4 +1,3 @@
-import * as React from "react"
 
 const Arrow = (props) => (
   <svg
