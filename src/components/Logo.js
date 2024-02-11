73a909aa28405276bd927c49535dff37e3759ca1
abhijeet0401 <vayuabhijeet@gmail.com>
2024-02-11 23:11:37 +0530

    fix style import

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import LOGO from "../assets/logo.png";
-
+import {style} from "../styles/styles"
 const LogoText = styled.h1`
   font-family: "Akaya Telivigala", cursive;
   font-size: ${(props) => props.theme.fontxxxl};
